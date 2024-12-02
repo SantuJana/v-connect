70af1ebe4ffcdfc51ecd2696c70274139349a896
santu jana <santu.bluehorse@gmail.com>
2024-12-02 22:09:24 +0530

    home page responsive

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { IoEllipsisVertical, IoNotificationsOutline } from "react-icons/io5";
+import { HiMenuAlt2 } from "react-icons/hi";
 
 export default function TopBar() {
   return (
-    <div className="h-16 w-full px-4 shadow-md flex justify-between items-center">
-      <h3 className="text-violet-700 font-medium text-xl">Hi, Santu Jana</h3>
+    <div className="h-14 sm:h-16 w-full px-3 sm:px-4 shadow-md flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <HiMenuAlt2 className="text-xl flex sm:hidden text-violet-800"/>
+        <h3 className="text-violet-700 font-medium sm:text-xl text-pretty">
+          Hi, Santu Jana
+        </h3>
+      </div>
       <div className="flex items-center gap-3">
-        <h3 className="text-violet-700 text-2xl font-bold">Vconnect</h3>
-        <input type="text" name="search" id="search" placeholder="Search" className="px-5 py-2 rounded-full outline-violet-700 text-slate-700 shadow-md" />
-        <IoNotificationsOutline size={30} className="text-slate-700 cursor-pointer" />
-        <IoEllipsisVertical size={30} className="text-slate-700 cursor-pointer" />
+        <h3 className="text-violet-700 text-lg sm:text-2xl font-semibold sm:font-bold">
+          Vconnect
+        </h3>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search"
+          className="hidden sm:flex px-5 py-2 rounded-full outline-violet-700 text-slate-700 shadow-md"
+        />
+        <IoNotificationsOutline className="text-2xl sm:text-3xl text-slate-700 cursor-pointer" />
+        <IoEllipsisVertical className="text-2xl sm:text-3xl text-slate-700 cursor-pointer" />
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,23 +10,32 @@ export default function Home() {
       <SideBar />
       <div className="w-full flex flex-col">
         <TopBar />
-        <div className="h-full w-full p-2 overflow-y-auto">
-          <div className="p-2">
-            <p className="text-lg text-violet-700 font-semibold">Recent</p>
+        <div className="h-full w-full p-2 overflow-y-auto flex">
+          {/* User section */}
+          <div className="w-full">
+            <div className="p-1 sm:p-2">
+              <p className="text-md sm:text-lg text-violet-700 font-semibold">Recent</p>
+            </div>
+            <div className="p-1 sm:p-2 grid sm:flex gap-4 grid-cols-2 sm:flex-wrap">
+              {[...Array(5)].map((_, index: number) => (
+                <UserCard key={index} />
+              ))}
+            </div>
+            <div className="p-1 sm:p-2 flex gap-2 items-center">
+              <p className="text-md sm:text-lg text-violet-700 font-semibold">Suggested</p>
+              <IoReload className="text-lg sm:text-2xl text-violet-700 cursor-pointer" />
+            </div>
+            <div className="p-1 sm:p-2 grid sm:flex gap-4 grid-cols-2 sm:flex-wrap pb-4">
+              {[...Array(6)].map((_, index: number) => (
+                <UserCard key={index} />
+              ))}
+            </div>
           </div>
-          <div className="p-2 flex gap-4 flex-wrap">
-            {[...Array(5)].map((_, index: number) => (
-              <UserCard key={index} />
-            ))}
-          </div>
-          <div className="p-2 flex gap-2 items-center">
-            <p className="text-lg text-violet-700 font-semibold">Suggested</p>
-            <IoReload size={20} className="text-violet-700 cursor-pointer" />
-          </div>
-          <div className="p-2 flex gap-4 flex-wrap">
-            {[...Array(12)].map((_, index: number) => (
-              <UserCard key={index} />
-            ))}
+          {/* Notice and add section */}
+          <div className="hidden sm:flex justify-center items-center p-8">
+              <div className="ring-2 ring-violet-300 rounded-lg shadow-lg w-52 min-h-[80%] bg-slate-100">
+
+              </div>
           </div>
         </div>
       </div>
